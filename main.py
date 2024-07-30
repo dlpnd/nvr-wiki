de1b949ccb225cfe59480146a3d9136167de5315
dlpnd <dark0lord@yandex.ru>
2024-07-31 11:19:54 +1200

    Refactor code to have types

--- main.py
+++ main.py
@@ -22,43 +22,41 @@ def save_file(filename: str, content: str, mode: str) -> None:
         file.write(content)
 
 
-def get_str(value: Any) -> str | None:
+def get_str(value: Any) -> str:
     if isinstance(value, (bool, toml.decoder.CommentValue)):
         return str(value).lower()
     elif isinstance(value, (int, float)):
         return str(value)
     elif isinstance(value, str):
         return f"'{value}'"
+    else:
+        return str()
 
 
 with open("dx.md", "r") as file:
     dx_md = file.read()
 
 
-def write_main_settings(main_config: dict[str, Any]) -> None:
+def write_main_settings(main_config: dict[dict[str, dict[str, str]], dict[str, dict[str, toml.decoder.CommentValue]]]) -> None:
     save_file(MAIN_SAVE_LOCATION, "", "w")
     save_file(MAIN_SAVE_LOCATION, MAIN_HEADER, "a")
     save_file(MAIN_SAVE_LOCATION, MAIN_SUB_HEADER, "a")
 
     for menu_items, section_config in main_config.items():
-        save_file(MAIN_SAVE_LOCATION,
-                  f"{HEADING[2]}{menu_items}{NEW_LINE}", "a")
+        save_file(MAIN_SAVE_LOCATION, f"{HEADING[2]}{menu_items}{NEW_LINE}", "a")
         save_file(MAIN_SAVE_LOCATION, UNDERLINE, "a")
 
         individual_menu = section_config.items()
         for sub_menu, sub_menu_values in individual_menu:
             if menu_items == "Menu" and sub_menu == "Keys":
                 save_file(MAIN_SAVE_LOCATION, f"{dx_md}{NEW_LINE}", "a")
-            save_file(MAIN_SAVE_LOCATION,
-                      f"{HEADING[3]}{sub_menu}{NEW_LINE}", "a")
+            save_file(MAIN_SAVE_LOCATION, f"{HEADING[3]}{sub_menu}{NEW_LINE}", "a")
 
             ini_config = sub_menu_values.items()
             for key, value in ini_config:
-                save_file(MAIN_SAVE_LOCATION,
-                          f"{HEADING[4]}{key}{NEW_LINE}", "a")
+                save_file(MAIN_SAVE_LOCATION, f"{HEADING[4]}{key}{NEW_LINE}", "a")
                 if hasattr(value, "val"):
-                    save_file(MAIN_SAVE_LOCATION,
-                              f"{value.comment[3:]}{NEW_LINE}", "a")
+                    save_file(MAIN_SAVE_LOCATION, f"{value.comment[3:]}{NEW_LINE}", "a")
                     save_file(
                         MAIN_SAVE_LOCATION, f"{DEFAULT}{get_str(value.val)}{NEW_LINE}{UNDERLINE}", "a")
                 else:
@@ -66,33 +64,26 @@ def write_main_settings(main_config: dict[str, Any]) -> None:
                         MAIN_SAVE_LOCATION, f"{DEFAULT}{get_str(value)}{NEW_LINE}{UNDERLINE}", "a")
 
 
-def write_shader_settings(shader_config: dict[str, Any]) -> None:
+def write_shader_settings(shader_config: dict[dict[str, dict[str, str]], dict[str, dict[str, toml.decoder.CommentValue]]]) -> None:
     for menu_items, section_config in shader_config.items():
         save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", "", "w")
-        save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}",
-                  f"{HEADING[1]}{menu_items}{NEW_LINE}", "a")
+        save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", f"{HEADING[1]}{menu_items}{NEW_LINE}", "a")
         save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", UNDERLINE, "a")
 
         individual_menu = section_config.items()
         for sub_menu, sub_menu_values in individual_menu:
-            save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}",
-                      f"{HEADING[2]}{sub_menu}{NEW_LINE}", "a")
+            save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", f"{HEADING[2]}{sub_menu}{NEW_LINE}", "a")
 
             ini_config = sub_menu_values.items()
             for key, value in ini_config:
-                save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}",
-                          f"{HEADING[3]}{key}{NEW_LINE}", "a")
+                save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", f"{HEADING[3]}{key}{NEW_LINE}", "a")
                 if hasattr(value, "val"):
-                    save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}",
-                              f"{value.comment[3:]}{NEW_LINE}", "a")
-                    save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}",
-                              f"{DEFAULT}{get_str(value.val)}{NEW_LINE}{UNDERLINE}", "a")
+                    save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", f"{value.comment[3:]}{NEW_LINE}", "a")
+                    save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", f"{DEFAULT}{get_str(value.val)}{NEW_LINE}{UNDERLINE}", "a")
                 else:
-                    save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}",
-                              f"{DEFAULT}{get_str(value)}{NEW_LINE}{UNDERLINE}", "a")
+                    save_file(f"{SHADERS_SAVE_LOCATION}{menu_items}", f"{DEFAULT}{get_str(value)}{NEW_LINE}{UNDERLINE}", "a")
 
 
-nvr_config_toml = toml.load(
-    "NewVegasReloaded.dll.defaults.toml", decoder=toml.TomlPreserveCommentDecoder())
+nvr_config_toml = toml.load("NewVegasReloaded.dll.defaults.toml", decoder=toml.TomlPreserveCommentDecoder())
 write_main_settings(nvr_config_toml[MAIN])
 write_shader_settings(nvr_config_toml[SHADERS])
